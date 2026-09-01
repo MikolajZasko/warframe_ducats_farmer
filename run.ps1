7fb619f29557b4cd3b9f8d0925ec8c002c6b2cb7
MikolajZasko <mikolaj.zasko890290@gmail.com>
2026-09-01 18:10:23 +0200

    updated the main entry point

--- run.ps1
+++ run.ps1
@@ -1,17 +1,8 @@
 # activate virtual environment
 .\.venv\Scripts\activate
 
-# prep console script
-python scripts/console_prep.py
-
-# gather items info using the first python script
-python scripts/item_info_json_fetch.py
-
-# # gather links using the second python script
-# python scripts/primeJunk_get_links.py
-
-# scrape the market in search of best deals
-python scripts/primeJunk_v4.py
+# run the simple_ui script
+python scripts/simple_ui.py
 
 # deactivate virtual environment
 deactivate
